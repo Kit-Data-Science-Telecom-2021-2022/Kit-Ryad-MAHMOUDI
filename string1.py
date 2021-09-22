601fb50a5b83e13fdc6b1a9bdb289d3060bc29c1
Ryad <ryadm4@hotmail.fr>
2021-09-22 13:23:50 +0200

    Exercice 1

--- string1.py
+++ string1.py
@@ -54,8 +54,11 @@ def both_ends(s):
 # Hint: s.replace(stra, strb) returns a version of string s
 # where all instances of stra have been replaced by strb.
 def fix_start(s):
-  # +++your code here+++
-  return
+  if len(s) >=1:
+  	premier = s[0]
+  	s=s.replace(premier,'*')
+  	s=premier+s[1:]
+  return s
 
 
 # D. MixUp
